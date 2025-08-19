1da2688578a1bbc6970d529364c61a62699ee641
Silje32 <msh070425@gmail.com>
2025-08-19 09:23:36 +0200

    Started the project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [list, setList] = useState([]);
+
+  const updateList = () => {
+    setList([...list, text]);
+    setText("");
+    console.log(list);
+  };
+
+  return (
+    <>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => {
+          setText(e.target.value);
+          console.log(text);
+        }}
+      />
+      <button onClick={updateList}>add</button>
+      {list.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+      text
+    </>
+  );
+}
+
+export default App;
